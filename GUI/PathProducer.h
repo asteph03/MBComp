b67d3358c1208394d7bd46fadc8d2d251d3eecec
asteph03 <122556343+asteph03@users.noreply.github.com>
2023-10-04 13:38:21 -0400

    Add files via upload

---
+++ GUI/PathProducer.h
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+    PathProducer.h
+    Created: 26 Sep 2023 1:34:17pm
+    Author:  Aidan
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+#include "FFTDataGenerator.h"
+#include "AnalyzerPathGenerator.h"
+#include "../PluginProcessor.h"
+
+struct PathProducer
+{
+    PathProducer(SingleChannelSampleFifo<MBCompAudioProcessor::BlockType>& scsf) :
+        leftChannelFifo(&scsf)
+    {
+        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+    }
+    void process(juce::Rectangle<float> fftBounds, double sampleRate);
+    juce::Path getPath() { return leftChannelFFTPath; }
+
+    void updateNegativeInfinity(float nf) { negativeInfinity = nf; }
+private:
+    SingleChannelSampleFifo<MBCompAudioProcessor::BlockType>* leftChannelFifo;
+
+    juce::AudioBuffer<float> monoBuffer;
+
+    FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+
+    juce::Path leftChannelFFTPath;
+
+    float negativeInfinity{ -48.f };
+};
